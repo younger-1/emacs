c404316b10cab1b6e61d719ea5875fe99904d48f
Xavier Young <45989017+younger-1@users.noreply.github.com>
2025-08-26 00:42:46 +0800

    new: god-mode

--- init.el
+++ init.el
@@ -441,6 +441,7 @@
   ;; buffer
   (setq uniquify-buffer-name-style 'forward)
   (setq switch-to-buffer-obey-display-actions t)
+  (setq switch-to-buffer-in-dedicated-window 'pop)
   ;; Keep the compilation buffer in the background, except when there's an error
   (add-to-list 'display-buffer-alist
                '("\\*.*compilation\\*" (display-buffer-no-window)))
@@ -1547,6 +1548,57 @@ makes it easier to edit it."
           mwheel-scroll))
   (keyfreq-mode +1)
   (keyfreq-autosave-mode +1))
+
+(use-package god-mode
+  :defer 0.5
+  :bind (("<escape>" . god-local-mode)
+         :map god-local-mode-map
+         ("." . #'repeat))
+  :init
+  (setq god-mode-enable-function-key-translation nil)
+  :config
+  (god-mode)
+  ;; Visual indicators for God mode
+  (custom-set-faces
+   '(god-mode-lighter ((t (:inherit error)))))
+  ;;
+  (defun xy/god-mode-update-cursor-type ()
+    (setq cursor-type (if (or god-local-mode buffer-read-only) 'box 'bar)))
+  (add-hook 'post-command-hook #'xy/god-mode-update-cursor-type)
+  ;;
+  ;; (defun xy/god-mode-update-mode-line ()
+  ;;   (cond
+  ;;    (god-local-mode
+  ;;     (set-face-attribute 'mode-line nil
+  ;;                         :foreground "#604000"
+  ;;                         :background "#fff29a")
+  ;;     (set-face-attribute 'mode-line-inactive nil
+  ;;                         :foreground "#3f3000"
+  ;;                         :background "#fff3da"))
+  ;;    (t
+  ;;     (set-face-attribute 'mode-line nil
+  ;;       		  :foreground "#0a0a0a"
+  ;;       		  :background "#d7d7d7")
+  ;;     (set-face-attribute 'mode-line-inactive nil
+  ;;       		  :foreground "#404148"
+  ;;       		  :background "#efefef"))))
+  (defun xy/god-mode-update-mode-line ()
+    (cond (god-local-mode
+           (progn
+             (set-face-background 'mode-line "#e9e2cb")
+             (set-face-foreground 'mode-line "black")
+             (set-face-background 'mode-line-inactive "#e9e2cb")
+             (set-face-foreground 'mode-line-inactive "black")))
+          (t (progn
+               (set-face-background 'mode-line "#0a2832")
+               (set-face-foreground 'mode-line "white")
+               (set-face-background 'mode-line-inactive "#0a2832")
+               (set-face-foreground 'mode-line-inactive "white")))))
+  (add-hook 'post-command-hook #'xy/god-mode-update-mode-line)
+  ;; isearch integration
+  (require 'god-mode-isearch)
+  (define-key isearch-mode-map (kbd "<escape>") #'god-mode-isearch-activate)
+  (define-key god-mode-isearch-map (kbd "<escape>") #'god-mode-isearch-disable))
 
 
 ;;; buffer
