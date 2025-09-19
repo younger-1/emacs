f405e358fcad9afe8d340061913aef87e3481267
Xavier Young <45989017+younger-1@users.noreply.github.com>
2025-09-20 00:39:39 +0800

    new: evil

--- init.el
+++ init.el
@@ -1656,55 +1656,43 @@ makes it easier to edit it."
   (keyfreq-mode +1)
   (keyfreq-autosave-mode +1))
 
-(use-package god-mode
+(use-package evil
   :defer 0.5
-  :bind (("<escape>" . god-local-mode)
-         :map god-local-mode-map
-         ("." . #'repeat))
-  :init
-  (setq god-mode-enable-function-key-translation nil)
   :config
-  (god-mode)
-  ;; Visual indicators for God mode
-  ;; (set-face-attribute 'god-mode-lighter nil :inherit 'match)
-  ;;
-  (defun xy/god-mode-update-cursor-type ()
-    (setq cursor-type (if (or god-local-mode buffer-read-only) 'box 'bar)))
-  (add-hook 'post-command-hook #'xy/god-mode-update-cursor-type)
-  ;;
-  ;; (defun xy/god-mode-update-mode-line ()
-  ;;   (cond
-  ;;    (god-local-mode
-  ;;     (set-face-attribute 'mode-line nil
-  ;;                         :foreground "#604000"
-  ;;                         :background "#fff29a")
-  ;;     (set-face-attribute 'mode-line-inactive nil
-  ;;                         :foreground "#3f3000"
-  ;;                         :background "#fff3da"))
-  ;;    (t
-  ;;     (set-face-attribute 'mode-line nil
-  ;;       		  :foreground "#0a0a0a"
-  ;;       		  :background "#d7d7d7")
-  ;;     (set-face-attribute 'mode-line-inactive nil
-  ;;       		  :foreground "#404148"
-  ;;       		  :background "#efefef"))))
-  (defun xy/god-mode-update-mode-line ()
-    (cond (god-local-mode
-           (progn
-             (set-face-background 'mode-line "#e9e2cb")
-             (set-face-foreground 'mode-line "black")
-             (set-face-background 'mode-line-inactive "#e9e2cb")
-             (set-face-foreground 'mode-line-inactive "black")))
-          (t (progn
-               (set-face-background 'mode-line "#0a2832")
-               (set-face-foreground 'mode-line "white")
-               (set-face-background 'mode-line-inactive "#0a2832")
-               (set-face-foreground 'mode-line-inactive "white")))))
-  (add-hook 'post-command-hook #'xy/god-mode-update-mode-line)
-  ;; isearch integration
-  (require 'god-mode-isearch)
-  (define-key isearch-mode-map (kbd "<escape>") #'god-mode-isearch-activate)
-  (define-key god-mode-isearch-map (kbd "<escape>") #'god-mode-isearch-disable))
+  (evil-mode +1)
+  :init
+  ;; `evil-toggle-key' is "C-z"
+  (setq evil-want-integration t)
+  (setq evil-want-keybinding nil)
+
+  (setq evil-disable-insert-state-bindings t)
+  (setq evil-want-minibuffer t)
+
+  (setq evil-want-C-u-delete t)
+  (setq evil-want-C-u-scroll t)
+  (setq evil-want-Y-yank-to-eol t)
+
+  (setq evil-shift-width 2)
+  (setq evil-move-beyond-eol t)
+  (setq evil-cross-lines t)
+  (setq evil-respect-visual-line-mode t)
+  (setq evil-split-window-below t)
+  (setq evil-vsplit-window-right t)
+
+  ;; Enable text object match, e.g. kbd::gn
+  (setq evil-search-module 'evil-search)
+  ;; @note: An underscore _ is a word character in Vim, but not in emacs
+  ;; so Evil use kbd::o as symbol object, making kbd::cio a good alternative to Vim’s kbd::ciw
+  (setq evil-symbol-word-search t)
+
+  ;; (setq evil-want-fine-undo t)
+  (setopt evil-undo-system 'undo-redo)
+  (with-eval-after-load 'undo-fu
+    (setopt evil-undo-system 'undo-fu))
+
+  (setq evil-mode-line-format '(before . mode-line-frame-identification))
+  :config
+  (setq display-line-numbers-type 'visual))
 
 
 ;;; buffer
